acf7d2bbb47323b33e8b90e2aa303d901db03f24
confessionsddn <itmconfessionddn@gmail.com>
2026-02-18 19:39:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   build: {
-    outDir: 'dist',
     rollupOptions: {
-      output: {
-        manualChunks: undefined
+      input: {
+        main: './index.html'
       }
-    }
+    },
+    // Don't hash OneSignal worker files
+    assetsInlineLimit: 0,
+    chunkSizeWarningLimit: 1000
   }
 })
