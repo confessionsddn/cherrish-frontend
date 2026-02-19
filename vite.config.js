5293e4870c11568f6c49e9537a86a88293454081
confessionsddn <itmconfessionddn@gmail.com>
2026-02-19 20:40:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,8 @@ export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
   build: {
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    },
-    // Don't hash OneSignal worker files
-    assetsInlineLimit: 0,
-    chunkSizeWarningLimit: 1000
+    outDir: 'dist',
+    assetsDir: 'assets',
+    copyPublicDir: true
   }
 })
